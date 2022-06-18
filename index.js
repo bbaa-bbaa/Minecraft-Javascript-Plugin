672b5174a5b1c8889ea96327fe1efcdc1169c24e
bbaa <bbaa@bbaa.fun>
2022-06-18 15:45:26 +0800

    233

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let PluginCore = require("./core/PluginCore");
 const fs = require("fs-extra");
 global.FOA = new PluginCore({
-  Rcon: { host: "127.0.0.1", port: 25575, password: "zyg" },
-  BaseDir: "/home/bbaa/mc118/",
-  newVersion:true
+  Rcon: { host: "127.0.0.1", port: 25575, password: "bbaa" },
+  BaseDir: "/home/bbaa/EnigTech/",
+  newVersion:false
 });
 let List = fs.readdirSync(__dirname + "/plugins");
 for (let Constructor of List) {
